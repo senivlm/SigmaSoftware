8511940f40ae5a9f401195fae3f6c4a9c1679be7
PetrovAV <petrovav85@gmail.com>
2022-05-21 20:46:44 +0200

    add Matrixfiller

--- task2ex2/Program.cs
+++ task2ex2/Program.cs
@@ -6,44 +6,27 @@ namespace task2Ex2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Write sqrt matrix lenght");
+            int n = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[n, n];
+            MatrixFiller.FillLikeASnake(matrix);
+            PrintMatrix(matrix);
 
         }
 
-        static void Main2(string[] args)
+        static void PrintMatrix(int[,] matrix)
         {
-            int n = 7;
-            int[,] arr = new int[n, n];
-
-            int i = 0;
-            int j = 0;
-            arr[0, 0] = 1;
-            arr[n - 1, n - 1] = n * n;
-            bool up = true;
-            int increment = 1;
-            for (int value = 1; value < n * n; value++)
+            for (int i = 0; i < matrix.GetLength(0); i++)
             {
-                if (up)
-                    if (i > 0) { i--; j++; }
-                    else { j++; up = false; }
-                else
-                    if (j > 0) { i++; j--; }
-                else { i++; up = true; }
-                if (n % 2 == 0 & i == n | n % 2 != 0 & j == n) break;
-
-                arr[i, j] = value + 1;
-                arr[n - i - 1, n - j - 1] = n * n - value;
-            }
-
-            for (i = 0; i < n; i++)
-            {
-                for (j = 0; j < n; j++)
+                for (int j = 0; j < matrix.GetLength(1); j++)
                 {
-                    Console.Write((arr[i, j] + "     ").Substring(0, 3));
+                    Console.Write((matrix[i, j] + "     ").Substring(0, 3));
                 }
                 Console.WriteLine("");
                 Console.WriteLine("");
             }
+
+
         }
     }
 

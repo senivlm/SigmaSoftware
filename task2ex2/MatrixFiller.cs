41368147355ea768c31e83b719f83d1305f47581
PetrovAV <petrovav85@gmail.com>
2022-05-21 23:17:59 +0200

    fix

---
+++ task2ex2/MatrixFiller.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace task2ex2
+{
+    public class MatrixFiller
+    {
+        private MatrixFiller()
+        {
+        }
+
+        public static void FillLikeASnake(int[,] matrix)
+        {
+            int n = matrix.GetLength(0);
+            int i = 0;
+            int j = 0;
+            matrix[0, 0] = 1;
+            matrix[n - 1, n - 1] = n * n;
+            bool up = true;
+
+            for (int value = 1; value < n * n; value++)
+            {
+                if (up)
+                    if (i > 0) { i--; j++; }
+                    else { j++; up = false; }
+                else
+                    if (j > 0) { i++; j--; }
+                else { i++; up = true; }
+                if (n % 2 == 0 & i == n | n % 2 != 0 & j == n) break;
+
+                matrix[i, j] = value + 1;
+                matrix[n - i - 1, n - j - 1] = n * n - value;
+
+            }
+
+           
+        }
+    }
+}
---
+++ task2ex2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace task2ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Set Lenght of sqrt matrix");
+            int n= int.Parse(Console.ReadLine());
+            int[,] matrix = new int[n, n];
+            MatrixFiller.FillLikeASnake(matrix);
+            Console.WriteLine("2");
+            PrintMatrix(matrix);
+
+        }
+        static void PrintMatrix(int[,] matrix)
+        {
+            int n1 = matrix.GetLength(0);
+            int n2 = matrix.GetLength(1);
+            for (int i=0; i<n1; i++) {
+                for (int j = 0; j < n2; j++)
+                {
+                    Console.Write(((String)(matrix[i, j].ToString() + "   ")).Substring(0, 3));
+                }
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+        }
+    }
+}

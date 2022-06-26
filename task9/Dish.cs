bd65cd4f3f043e1b77b6377189be662a04e1e52a
PetrovAV <petrovav85@gmail.com>
2022-06-26 16:22:36 +0200

    task9

---
+++ task9/Dish.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace task9
+{
+    public class Dish
+    {
+        Dictionary<string, int> _ingredients;
+
+        public Dish()
+        {
+            _ingredients = new Dictionary<string, int>();
+        }
+
+        public IEnumerable<string> keys => _ingredients.Keys;
+
+        public int this[String key]
+        {
+            get => _ingredients[key];
+        }
+
+        public void add(String ingr, int weight)
+        {
+            _ingredients.Add(ingr, weight);
+        }
+    }
+}
---
+++ task9/Order.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace task9
+{
+    public class Order
+    {
+        Dictionary<String, int> order;
+
+        public Order()
+        {
+            order = new Dictionary<string, int>();
+        }
+
+        public IEnumerable<string> keys => order.Keys;
+
+        public int this[string key]
+        {
+            get => order[key];
+        }
+
+        public void add(string key, int value)
+        {
+            order.Add(key, value);
+        }
+    }
+}

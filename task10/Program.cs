41b9da7bc56c2a8d154f200cd4b5e56921982927
PetrovAV <petrovav85@gmail.com>
2022-06-26 19:40:17 +0200

    task10

---
+++ task10/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace task10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> dictionary;
+            List<string> text;
+            try
+            {
+                text = Reader.ReadText("../../../Text.txt");
+                dictionary = Reader.ReadDictionary("../../../Dictionary.txt");
+                Translator translator = new Translator();
+                translator.AddDictionary(dictionary);
+                foreach (string i in text)
+                {
+                    translator.AddText(i);
+                }
+                string changedText = translator.ChangeWords();
+                Console.WriteLine(changedText);
+                Reader.WriteNewLineToFile(changedText, "../../../Result.txt");
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("FileNotFoundException");
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("ArgumentException");
+            }
+            catch (WordDoesntFound)
+            {
+                Console.WriteLine("WordDoesntFound");
+            }
+
+        }
+    }
+}
---
+++ task10/Translator.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace task10
+{
+    public class Translator
+    {
+        Dictionary<string, string> vocabluary;
+        string text;
+        string pathToText;
+        string pathToDictionary;
+
+
+        public Translator() : this("../../../Text.txt", "../../../Dictionary.txt")
+        {
+        }
+        public Translator(string pathToText, string pathToDictionary) : this(pathToText, pathToDictionary, new Dictionary<string, string>(), "")
+        {
+        }
+
+        public Translator(string pathToText, string pathToDictionary, Dictionary<string, string> vocabluary, string text)
+        {
+            this.pathToText = pathToText;
+            this.pathToDictionary = pathToDictionary;
+            this.vocabluary = vocabluary;
+            this.text = text;
+
+        }
+
+        public void AddText(string text)
+        {
+            this.text += text;
+        }
+
+        public void AddDictionary(Dictionary<string, string> vocabluary)
+        {
+            this.vocabluary = vocabluary;
+        }
+
+        string SetCase(string originalWord, string Exampleword)
+        {
+            //корректно проверять только 1 символ
+            string result = originalWord;
+            string firstCharInExample = Exampleword[0].ToString();
+            if (firstCharInExample.Equals(firstCharInExample.ToUpper()))
+            {
+                result = originalWord[0].ToString().ToUpper() + originalWord[1..];
+            }
+            return result;
+        }
+
+        string ChangeWord(string word)
+            {
+            string result = "";
+            string tempword = word;
+            if (!word.Trim().Equals(""))
+            {
+                char lastChar = word[word.Length - 1];
+                if (Char.IsPunctuation(lastChar))
+                {
+                    if (word.Length == 1)
+                    {
+                        tempword = ""+lastChar;
+                    }
+                    else
+                    {
+                        //рекурсивный вызов для ... и !? 
+                        tempword = ChangeWord(word[0..^1])+ lastChar;
+                    }
+                }
+                else
+                {
+                    string wordInLowerCase = word.ToLower();
+                    if (!vocabluary.ContainsKey(wordInLowerCase))
+                    {
+                        AddToDictionary(wordInLowerCase);
+                    }
+                    tempword = SetCase(vocabluary[wordInLowerCase],word);
+
+                }
+            }
+            result += tempword;
+
+            return result;
+            }
+
+
+
+        public string ChangeWords()
+        {
+            string result = "";
+            char delimiter = ' ';
+            var words = text.Split(delimiter);
+            foreach (string word in words)
+            {
+               result += ChangeWord(word)+delimiter;
+            }
+            result = result[0..^1];
+            return result;
+        }
+
+        public void AddToDictionary(string word)
+        {
+            Console.WriteLine($"Enter translate of {word}");
+            string translated = Console.ReadLine();
+            vocabluary.Add(word, translated);
+            Reader.WriteToDictionary(word, translated, "../../../Dictionary.txt");
+        }
+    }
+}

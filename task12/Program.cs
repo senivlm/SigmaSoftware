d56f2a22622354a1513712ff28b4b1172c244c51
PetrovAV <petrovav85@gmail.com>
2022-07-02 16:14:07 +0200

    add 12

---
+++ task12/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace task12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
